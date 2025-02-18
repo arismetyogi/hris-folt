e187e995aea9460a47dfc5b041c78e69a2f2a6a7
arismetyogi <arismetyogi@gmail.com>
2025-02-18 10:53:21 +0700

    disable bulk delete when there is no checked data

--- app/Livewire/UserTable.php
+++ app/Livewire/UserTable.php
@@ -66,6 +66,7 @@ final class UserTable extends PowerGridComponent
         return [
             Button::add('bulk-delete')
                 ->slot('Bulk delete (<span x-text="window.pgBulkActions.count(\'' . $this->tableName . '\')"></span>)')
+                ->attributes(['x-bind:disabled' => "window.pgBulkActions.count('" . $this->tableName . "') === 0"])
                 ->class('pg-btn-white dark:ring-pg-primary-600 dark:border-pg-primary-600 dark:hover:bg-pg-primary-700 dark:ring-offset-pg-primary-800 dark:text-pg-primary-300 dark:bg-pg-primary-700')
                 ->dispatch('bulkDelete.' . $this->tableName, []),
         ];
