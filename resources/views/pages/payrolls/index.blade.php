dac56bdde7e09debcd2c4748486b3cb07601a35c
arismetyogi <arismetyogi@gmail.com>
2025-02-24 14:17:08 +0700

    create payrolls index

---
+++ resources/views/pages/payrolls/index.blade.php
@@ -0,0 +1,22 @@
+<?php
+
+use function Laravel\Folio\{middleware, name};
+use Livewire\Volt\Component;
+
+middleware(['auth', 'verified', 'can:'.\App\Enums\Permissions::ManagePayrolls->value]);
+name('payrolls.index');
+
+?>
+
+
+<x-layouts.app>
+    <x-app.heading
+        title="Payrolls"
+        description="Manage Data Payroll"
+        :border="false"
+    />
+
+    <div class="py-12">
+        @livewire('karyawan-table')
+    </div>
+</x-layouts.app>
