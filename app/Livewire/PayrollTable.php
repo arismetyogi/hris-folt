15876fa2bba6e21564f9033a928563a0e26fc429
arismetyogi <arismetyogi@gmail.com>
2025-02-24 14:28:11 +0700

    create payroll table and update payroll model fillable

---
+++ app/Livewire/PayrollTable.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Enums\Roles;
+use App\Models\Payroll;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
+use PowerComponents\LivewirePowerGrid\Button;
+use PowerComponents\LivewirePowerGrid\Column;
+use PowerComponents\LivewirePowerGrid\Facades\Filter;
+use PowerComponents\LivewirePowerGrid\Facades\PowerGrid;
+use PowerComponents\LivewirePowerGrid\PowerGridComponent;
+use PowerComponents\LivewirePowerGrid\PowerGridFields;
+
+final class PayrollTable extends PowerGridComponent
+{
+    public string $tableName = 'payroll-table';
+
+    public function setUp(): array
+    {
+        $this->showCheckBox();
+
+        return [
+            PowerGrid::header()
+                ->showSearchInput(),
+            PowerGrid::footer()
+                ->showPerPage()
+                ->showRecordCount(),
+        ];
+    }
+
+    public function datasource(): Builder
+    {
+        $user = auth()->user();
+        if ($user->hasRole(Roles::SuperAdmin->value)) {
+            return Payroll::query()->with('karyawan');
+        } else {
+            return Payroll::query()->with('karyawan')->whereHas('karyawan', function ($query) use ($user) {
+                $query->where('branch_id', $user->branch_id);
+            });
+        }
+    }
+
+    public function relationSearch(): array
+    {
+        return [];
+    }
+
+    public function fields(): PowerGridFields
+    {
+        return PowerGrid::fields()
+            ->add('id')
+            ->add('created_at_formatted', fn(Payroll $model) => Carbon::parse($model->created_at)->format('d/m/Y H:i:s'));
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make('Id', 'id'),
+            Column::make('Created at', 'created_at_formatted', 'created_at')
+                ->sortable(),
+
+            Column::action('Action')
+        ];
+    }
+
+    public function filters(): array
+    {
+        return [
+            Filter::datetimepicker('created_at'),
+        ];
+    }
+
+    #[
+        \Livewire\Attributes\On('edit')]
+    public function edit($rowId): void
+    {
+        $this->js('alert(' . $rowId . ')');
+    }
+
+    public function actions(Payroll $row): array
+    {
+        return [
+            Button::add('edit')
+                ->slot('Edit: ' . $row->id)
+                ->id()
+                ->class('pg-btn-white dark:ring-pg-primary-600 dark:border-pg-primary-600 dark:hover:bg-pg-primary-700 dark:ring-offset-pg-primary-800 dark:text-pg-primary-300 dark:bg-pg-primary-700')
+                ->dispatch('edit', ['rowId' => $row->id])
+        ];
+    }
+
+    /*
+    public function actionRules($row): array
+    {
+       return [
+            // Hide button edit for ID 1
+            Rule::button('edit')
+                ->when(fn($row) => $row->id === 1)
+                ->hide(),
+        ];
+    }
+    */
+}
--- app/Models/Payroll.php
+++ app/Models/Payroll.php
@@ -7,7 +7,10 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Payroll extends Model
 {
-    public function karyawanw(): BelongsTo
+    protected $fillable = [
+        'bln_thn', 'karyawan_id', '1050_honorarium', 'uang_saku_mb', '3000_lembur', '2580_tunj_lain', 'ujp', '4020_sumbangan_cuti_tahunan', '6500_pot_wajib_koperasi', '6540_pot_pinjaman_koperasi', '6590_pot_ykkkf', '6620_pot_keterlambatan', '6630_pinjaman_karyawan', '6700_pot_bank_mandiri', '6701_pot_bank_bri', '6702_pot_bank_btn', '6703_pot_bank_danamon', '6704_pot_bank_dki', '6705_pot_bank_bjb', '6750_pot_adm_bank_mandiri', '6751_pot_adm_bank_bri', '6752_pot_adm_bank_bjb', '6900_pot_lain',];
+
+    public function karyawan(): BelongsTo
     {
         return $this->belongsTo(Karyawan::class);
     }
